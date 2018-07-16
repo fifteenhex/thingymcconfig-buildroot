3a91769e519c9474bdb89989d469f6697ce23b51
Daniel Palmer <daniel@0x0f.com>
2018-07-17 01:22:51 +0900

    Binary should be installed by meson now.

--- package/thingymcconfig/thingymcconfig.mk
+++ package/thingymcconfig/thingymcconfig.mk
@@ -11,11 +11,11 @@ THINGYMCCONFIG_LICENSE = GPL-3.0
 THINGYMCCONFIG_LICENSE_FILES = LICENSE
 THINGYMCCONFIG_DEPENDENCIES = host-pkgconf libmicrohttpd json-glib libnl
 THINGYMCCONFIG_GIT_SUBMODULES = YES
+THINGYMCCONFIG_INSTALL_STAGING = YES
 
-THINGYMCCONFIG_CONF_OPTS += -DWPASUPPLICANT_BINARYPATH="/usr/sbin/wpa_supplicant"
+THINGYMCCONFIG_CONF_OPTS += -Dwpasupplicantpath=/usr/sbin/wpa_supplicant
 
 define THINGYMCCONFIG_INSTALL_TARGET_CMDS
-        $(INSTALL) -D -m 0755 $(@D)/thingymcconfig $(TARGET_DIR)/usr/sbin/thingymcconfig
 	$(INSTALL) -D -m 0755 $(THINGYMCCONFIG_PKGDIR)/S90thingymcconfig $(TARGET_DIR)/etc/init.d/S90thingmcconfig
 endef
 
