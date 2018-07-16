4778f87208b904931015d2ff4dc44c6a03df3e1b
Daniel Palmer <daniel@0x0f.com>
2018-07-17 00:56:12 +0900

    Update for meson

--- package/thingymcconfig/thingymcconfig.mk
+++ package/thingymcconfig/thingymcconfig.mk
@@ -12,13 +12,11 @@ THINGYMCCONFIG_LICENSE_FILES = LICENSE
 THINGYMCCONFIG_DEPENDENCIES = host-pkgconf libmicrohttpd json-glib libnl
 THINGYMCCONFIG_GIT_SUBMODULES = YES
 
-define THINGYMCCONFIG_BUILD_CMDS
-        PKGCONFIG=$(PKG_CONFIG_HOST_BINARY) CC=$(TARGET_CROSS)gcc $(MAKE) WPASUPPLICANT_BINARYPATH=/usr/sbin/wpa_supplicant -C $(@D)
-endef
+THINGYMCCONFIG_CONF_OPTS += -DWPASUPPLICANT_BINARYPATH="/usr/sbin/wpa_supplicant"
 
 define THINGYMCCONFIG_INSTALL_TARGET_CMDS
         $(INSTALL) -D -m 0755 $(@D)/thingymcconfig $(TARGET_DIR)/usr/sbin/thingymcconfig
 	$(INSTALL) -D -m 0755 $(THINGYMCCONFIG_PKGDIR)/S90thingymcconfig $(TARGET_DIR)/etc/init.d/S90thingmcconfig
 endef
 
-$(eval $(generic-package))
+$(eval $(meson-package))
